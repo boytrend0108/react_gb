c6a62bb8709b8ffd648bb8e4ceef31d22720c638
boytrend0108 <boytrend@gmail.com>
2023-08-19 18:47:10 +0300

    add comment

--- src/pages/Gists.js
+++ src/pages/Gists.js
@@ -1,58 +1,58 @@
- import {useState, useEffect, useCallback} from "react";
- import { API_URL_PUBLIC } from "../constants/gists";
- import { CircularProgress } from '@mui/material';
- 
- const Gists = () => {
+import { useState, useEffect, useCallback } from "react";
+import { API_URL_PUBLIC } from "../constants/gists";
+import { CircularProgress } from "@mui/material";
+
+const Gists = () => {
   const [gists, setGists] = useState([]);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   const requestGists = async () => {
-    setIsLoading(true)
-    
-    try{
+    setIsLoading(true);
+
+    try {
       const response = await fetch(API_URL_PUBLIC);
-      if (!response.ok) throw new Error(`Request failed with status ${response.status}!`);
+      if (!response.ok)
+        throw new Error(`Request failed with status ${response.status}!`);
       const result = await response.json();
       setGists(result);
-    }
-    catch(err) {
+    } catch (err) {
       setError(true);
       console.log(err);
+    } finally {
+      setIsLoading(false);
     }
-    finally {setIsLoading(false)} 
-  }
+  };
 
   useEffect(() => {
     requestGists();
   }, []);
 
   const renderGists = useCallback(
-    gist => <li key={gist.id}>{gist.description || 'no description'}</li>,
+    (gist) => <li key={gist.id}>{gist.description || "no description"}</li>,
     []
-  )
+  );
 
-if (isLoading) {
-  return (
-    <CircularProgress />
-  )
-}
+// render===========
+  if (isLoading) {
+    return <CircularProgress />;
+  }
 
-if (error) {
-  return (
-    <>
-      <p>Error</p>
-      <button onClick={requestGists}>Retry</button>
-    </>
-  )
-}
+  if (error) {
+    return (
+      <>
+        <p>Error</p>
+        <button onClick={requestGists}>Retry</button>
+      </>
+    );
+  }
 
   return (
     <ul>
-      {/* {gists.map(gist => <li key={gist.id}>{gist.description || 'no description'}</li>)} */} 
+      {/* {gists.map(gist => <li key={gist.id}>{gist.description || 'no description'}</li>)} */}
       {gists.map(renderGists)}
     </ul>
-  )
+  );
 };
 
 export default Gists;
