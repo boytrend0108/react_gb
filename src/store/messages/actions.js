29e12e8b841e2d250a372e954257c5b19d449381
boytrend0108 <boytrend@gmail.com>
2023-08-19 12:58:35 +0300

    ddd

--- src/store/messages/actions.js
+++ src/store/messages/actions.js
@@ -8,13 +8,3 @@ export const addMessage = (chatId, message) => ({
   payload: {chatId, message}
 })
 
-
-// ЗДЕСЬ АКTION ЭТО ФУНКЦИЯ- ПОЭТОМУ ИСПОЛЬЗУЕМ THUNK
-// export const addMessageWithThunk = (chatId, message) => (dispatch, getState) => {
-//   dispatch(addMessage(chatId, message));
-
-//   if (message.author !== "robot") {
-//     const botMessage =  {text:"I'm bot from thunk", author: 'robot'}
-//     setTimeout(() => {dispatch(addMessage(chatId, botMessage))}, 1700)
-//   }
-// }
