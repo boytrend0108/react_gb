cc69ddc7312c4c5b4dadd38cb9c868f7954728a5
boytrend0108 <boytrend@gmail.com>
2023-08-19 13:01:01 +0300

    fix middle ware

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,11 @@ import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import profileReduser from "./profile/reduser";
 import messagesReduser from './messages/reduser';
 import chatsReduser from './chats/reduser';
-// import middleware from '../middleware/middleware';
-import thunk from "redux-thunk"
+
+// подключаем миддлвару
+import middleware from '../middleware/middleware';
+
+
 
 // Объединяем все редюсеры 
 const redusers = combineReducers({
@@ -16,6 +19,6 @@ const composeEnhancer =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   redusers, // redusers или один редюсер без сombineREd..
-  composeEnhancer(applyMiddleware(thunk))
+  composeEnhancer(applyMiddleware(middleware))
   );
 export default store;
